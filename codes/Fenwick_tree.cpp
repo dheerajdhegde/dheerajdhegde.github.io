c34f0edf7105468b9ce4e443e3698d1b212957c3
dheerajdhegde <105264588+dheerajdhegde@users.noreply.github.com>
2024-07-09 02:20:35 +0530

    Create Fenwick_tree.cpp

---
+++ codes/Fenwick_tree.cpp
@@ -0,0 +1,36 @@
+#include <bits/stdc++.h>
+using namespace std;
+#define ll long long 
+
+ll arr[100005];
+ll bit[100005]={0};
+
+void update(ll i, ll val, ll n){
+    while(i<=n){
+        bit[i]+=val;
+        i=i+(i&(-i));
+    }
+}
+ll query(ll i){
+    ll sum=0;
+    while(i>0){
+        sum+=bit[i];
+        i=i-(i&(-i));
+    }
+    return sum;
+}
+int main() {
+    ll n;
+    cin>>n;
+    for(int i=1;i<=n;i++){//indexing always from 1
+        cin>>arr[i];        
+        update(i,arr[i],n);//build BIT
+    }
+    ll q,l,r;
+    cin>>q;
+    while(q--){
+        cin>>l>>r;
+        cout<<query(r)-query(l-1)<<endl;
+    }
+    
+}
